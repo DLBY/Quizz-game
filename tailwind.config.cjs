187f12fbe3b5a3f25cb7faa09dba62f3a3e483e1
DLBY <david.lobry96@gmail.com>
2023-01-24 11:10:15 +0100

    Config : Theme setup with Tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,37 @@
 /** @type {import('tailwindcss').Config} */
 
-const defaultTheme = require('tailwindcss/defaultTheme');
+const colors = require('tailwindcss/colors');
 
 module.exports = {
   mode: 'jit',
   purge: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  theme: {},
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      white: colors.white,
+      black: colors.black,
+      green: {
+        dark: '#012025',
+        DEFAULT: '#00C957',
+        secondary: '#019842',
+        pastel: '#067E7B',
+      },
+      blue: {
+        DEFAULT: '#4E8098',
+        light: '#90C2E7',
+      },
+      red: {
+        DEFAULT: colors.red[500],
+        alert: '#E74C3C',
+      },
+      purple: '#9B59B6',
+      sand: '#F9E79F',
+    },
+    fontFamily: {
+      sans: ['Poppins-Regular', 'sans-serif'],
+    },
+  },
   variants: {},
   plugins: [],
 };
